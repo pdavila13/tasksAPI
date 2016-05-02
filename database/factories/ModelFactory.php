3c56b7b82b332be4caa27b2a29288cdd2ffbe7d5
Paolo Davila <pdavila1313@gmail.com>
2016-05-02 16:48:44 +0200

    Added field for save api_token in ModelFactory and users_table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,5 +17,6 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'api_token' =>  sha1(uniqid(rand(), true)),
     ];
 });
